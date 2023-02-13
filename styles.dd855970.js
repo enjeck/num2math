52fd5015adc0578f3cf3b4a0237864da58e89a53
enjeck <patrathewhiz@gmail.com>
2023-02-13 22:35:15 +0100

    Updates

--- styles.dd855970.js
+++ styles.dd855970.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38617" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45747" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
